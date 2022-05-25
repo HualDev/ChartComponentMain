958a2e6dd6293149daccb46aabb2f43c8273151a
Hualdo <hucastillo@gmail.com>
2022-05-25 12:08:06 -0500

    Hover Final  Version

--- chart.js
+++ chart.js
@@ -5,6 +5,7 @@ xmlhttp.onload = function() {
   let day =document.getElementsByClassName('day')
   let graph =document.getElementsByClassName('graph')
   let spent = document.getElementsByClassName('spent')
+  let container = document.getElementsByClassName('spending_container')
 
   for(let i=0; i<myObj.length;i++ ){
     let height =myObj[i].amount
@@ -15,6 +16,17 @@ xmlhttp.onload = function() {
     if(height>=50){
       graph[i].style.backgroundColor='hsl(186, 34%, 60%)'
     }
+
+    container[i].addEventListener('mouseover',()=>{
+      spent[i].style.display='block'
+      graph[i].style.opacity='0.5'
+      container[i].style.cursor='pointer'
+    })
+
+    container[i].addEventListener('mouseout',()=>{
+      spent[i].style.display='none'
+      graph[i].style.opacity='1'
+    })
   }
   
 
